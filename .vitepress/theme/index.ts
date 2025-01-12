a83e1335f73d24064f8762a8443ebd5ab82908ec
LittleChest <hiya@littlew.top>
2025-01-12 16:34:18 +0800

    删除不再使用的依赖

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,13 +1,11 @@
 import DefaultTheme from "vitepress/theme";
 import "./style.css";
-import lang from "./lang.vue";
 
 import vitepressNprogress from "vitepress-plugin-nprogress";
 import "vitepress-plugin-nprogress/lib/css/index.css";
 
 export default {
   ...DefaultTheme,
-  lang,
   enhanceApp: (ctx) => {
     vitepressNprogress(ctx);
   },
